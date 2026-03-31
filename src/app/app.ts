6903f50d5cd8871a2d364ff5e014af25e92af6da
Ltheus <faco.computaria@gmail.com>
2026-03-30 23:03:57 -0300

    cria campos da ficha TUDO CAGADO

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FichaBaseComponent } from './pages/ficha-base/ficha-base.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  imports: [FichaBaseComponent],
 })
 export class App {
   protected readonly title = signal('ficha-mmilagres');
