2eb32bb490e4f7e883f525fa65e0d65b691437ba
Ltheus <faco.computaria@gmail.com>
2026-03-31 21:24:49 -0300

    reseta projeto para o basico, reove primeNG

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { FichaBaseComponent } from './pages/ficha-base/ficha-base.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.html',
   styleUrl: './app.css',
-  imports: [FichaBaseComponent],
 })
-export class App {
-  protected readonly title = signal('ficha-mmilagres');
-}
+export class App {}
